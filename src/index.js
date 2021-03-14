3e6f1d407f7371555336f345003cde73e1007c1b
Tanysha23 <tanysha.karpovich@gmail.com>
2021-03-14 22:13:53 +0300

    feat: min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return array && array.length ? Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return array && array.length ? Math.max(...array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array && array.length) {
+
+        var sum = 0;
+        for (var i = 0; i < array.length; i++) {
+            sum += parseInt(array[i], 10);
+        }
+
+        return sum / array.length;
+
+    }
+
+    return 0;
 }
